22c1ce99e533eb70e5c87fae96cb4dff2ed9c959
pnamunn <pnamunn@gmail.com>
2024-06-22 13:56:06 -0700

    PlayerList methods work

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <string>
 using namespace std;
 
 
@@ -29,15 +30,23 @@ class Deck : public Card {
                 cards.push_back(aCard);
             }
         }
-        cout << "\n Full deck created...";
+        cout << "Full deck created...\n";
     }
 
     // Card drawRandomCard() {
 
     // }
 
-    void removeCardFromDeck(Card card) {
 
+    void removeCardFromDeck() {
+        // int i = 0;
+        // while(i < 52) {
+        //     cout << cards[i].suit << " " << cards[i++].value << "\n";
+        // }
+  
+        int random = rand() % 52;
+        cout << cards[random].suit << " " << cards[random].value << "\n";
+        // cout << random;
     }
 
 };
@@ -49,26 +58,10 @@ class Player {
     public:
         string name;
         int chips;
-        Player *next, *last;
+        Player *next = NULL, *last = NULL;
 
     /* Constructor */
     Player() {}
-
-    void prompt(Player *player) {
-        int choice;
-        cout << player->name << "'s turn: \n";
-        cin >> choice;
-        switch(choice) {
-            case 0:
-                fold();
-            case 1:
-                call();
-            case 2:
-                raise();
-            default:
-                cout << "Not an option, try again";
-        }
-    }
 
     void fold(Player *player) {}
     void call(Player *player) {}
@@ -78,14 +71,81 @@ class Player {
             // player->showCard();
         }
     }
+
+    // void prompt(Player *player) {
+    //     int choice;
+    //     cout << player->name << "'s turn:\n";
+    //     cin >> choice;
+    //     switch(choice) {
+    //         case 0:
+    //             player.fold();
+    //         case 1:
+    //             call();
+    //         case 2:
+    //             raise();
+    //         default:
+    //             cout << "Not an option, try again";
+    //             prompt(player);
+    //     }
+    // }
+
 };
 
 
 class PlayerList {
-    Player *head, *tail, *curr;
+    public:
+        Player *head = NULL, *tail = NULL;
 
-    void addPlayer(Player *ptr) {}
-    void removePlayer(Player* ptr) {}
+        void addPlayer(int i) {
+            Player *player = new Player();
+            player->name = "Player" + to_string(i);
+
+            if(head == NULL) {     // first Player in the list
+                head = player;
+                tail = player;
+            }
+            else {
+                // Player *temp = tail;
+                tail->next = player;    // tail's next = new addition
+                player->last = tail;    // new addition's last = tail
+                tail = player;        // new addition becomes new tail
+            }
+            cout << player->name << " added\n";
+        }
+
+        void removePlayer(string name) {
+            Player *curr = head;
+            bool success = 0;
+            while(curr != NULL) {
+                if(curr->name == name) {
+                    if(curr->next == NULL) {    // removing the last player in the list
+                        curr->last->next = NULL;
+                    }
+                    else {
+                        curr->last->next = curr->next;
+                        curr->next->last = curr->last;
+                    }
+                    // outPlayer.addPlayer(&curr);
+                    cout << name << " removed\n";
+                    success = 1;
+                }
+                curr = curr->next;
+            }
+            if(!success)  cout << "That player name not found\n";
+        }
+
+        void listPlayers() {
+            if(head == NULL) {
+                cout << "Arrrg list be empty\n";
+            }
+            else {
+                Player *curr = head;
+                while(curr != NULL) {
+                    cout << curr->name << "\n";
+                    curr = curr->next;
+                }
+            }
+        }
 };
 
 
@@ -97,27 +157,47 @@ class Dealer {
 
     // shuffle()
     void flipCard() {
-        Card cardDrawn = Deck.drawRandomCard;
-        Deck.removeCardFromDeck(cardDrawn);
-        cout << 
+        // Card cardDrawn = Deck.drawRandomCard;
+        // Deck.removeCardFromDeck(cardDrawn);
+        // cout << 
     }
     // poolTotal()
     // compareHands()
+    // void dealCards(PlayerList *head) {
+    //     while()
+    // }
 
 };
 
 
 int main() {
-    // cout << "Hello";
+    srand(time(NULL));      // seed
+
     PlayerList inPlayer, outPlayer;
-    Deck deck;
+    int numPlayers;
+    cout << "Enter number of players: ";
+    cin >> numPlayers;
+
+    for (int i=1; i <= numPlayers; i++) {
+        inPlayer.addPlayer(i);
+    }
+
+    inPlayer.listPlayers();
+    inPlayer.removePlayer("Player2");
+    cout << "\n-\n";
+    inPlayer.listPlayers();
+    // cout << "\n-";
+
+
+    // Deck deck;
+    // deck.removeCardFromDeck();
 
 }
 
 
 // int main() {
 //     Dealer dealer;
-//     int numPlayers = 0;
+//     int numPlayers;
 
 //     cout << "Enter number of players: ";
 //     cin >> numPlayers;
